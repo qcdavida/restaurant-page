d48cf519659010daf69ca1f1a76734e944a7fdc9
David <dav_8612@live.com>
2022-04-22 11:57:14 -0400

    Adding latest code. Working on converting HTML code to plain Javascript. As well as making modules.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
 console.log("Hello, testing");
+console.log("working..");
+console.log("importing");
+import { header, mainContent } from './homePageModule';
+
+const contentDiv = document.getElementById('content');
+
+contentDiv.appendChild(header());
+contentDiv.appendChild(mainContent());
